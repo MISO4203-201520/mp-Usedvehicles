df4feb8e8b5d889536a054b5a3e9a4260589cf71
Harold Murcia <hl.murcia222@uniandes.edu.co>
2015-09-24 19:56:16 -0500

    #MUV-19 question colors improve

--- mpUsedVehicle.web/src/main/webapp/src/modules/messages/message.ctrl.js
+++ mpUsedVehicle.web/src/main/webapp/src/modules/messages/message.ctrl.js
@@ -2,13 +2,11 @@
     var mod = ng.module('messageModule');
 
     mod.controller('messageCtrl', ['CrudCreator', '$scope', 'messageService', 'messageModel','$location', 'authService', function (CrudCreator, $scope, svc, model, $location, authSvc) {
-            CrudCreator.extendController(this, svc, $scope, model, 'message');
-            
+            CrudCreator.extendController(this, svc, $scope, model, 'message');   
+    
         $scope.getQuestionsByProvider = function () {
             svc.getQuestionsByProvider(authSvc.getCurrentUser().id).then(function (Questions) {
-                        $scope.records = [];
-                        $scope.records.push(Questions);
-                        console.log("Ingresa Questions" + Questions.id);
+                    $scope.records = Questions;
             });
         };
             
