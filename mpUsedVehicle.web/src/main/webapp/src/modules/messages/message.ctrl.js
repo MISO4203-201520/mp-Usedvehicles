e3837d8636a70891152be3397f80bdf9db0458d1
Harold Murcia <hl.murcia222@uniandes.edu.co>
2015-09-26 23:17:32 -0500

    #MUV-19 current user role support

--- mpUsedVehicle.web/src/main/webapp/src/modules/messages/message.ctrl.js
+++ mpUsedVehicle.web/src/main/webapp/src/modules/messages/message.ctrl.js
@@ -1,7 +1,7 @@
 (function (ng) {
     var mod = ng.module('messageModule');
 
-    mod.controller('messageCtrl', ['CrudCreator', '$scope', 'messageService', 'messageModel','$location', 'authService' , function (CrudCreator, $scope, svc, model, $location, authSvc) {
+    mod.controller('messageCtrl', ['CrudCreator', '$scope', 'messageService', 'messageModel','$location', 'authService', 'userService' , function (CrudCreator, $scope, svc, model, $location, authSvc, svcUser) {
             CrudCreator.extendController(this, svc, $scope, model, 'message');   
     
         this.answer='';
@@ -21,6 +21,9 @@
         };
             
         $scope.getQuestionsByProvider();
+        
+        svcUser.getCurrentUser();
+        
 
         }]);
 })(window.angular);
