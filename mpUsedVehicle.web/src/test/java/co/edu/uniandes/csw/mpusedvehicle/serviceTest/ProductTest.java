37a3a0c4fe5355a9457e4ca50339b8654ffda938
f.wilmar10 <f.wilmar10@MISOEST-62.virtual.uniandes.edu.co>
2015-10-20 02:37:38 -0500

    Merge origin/MUV-64 into MUV-64
    Conflicts:
    	mpUsedVehicle.web/src/test/java/co/edu/uniandes/csw/mpusedvehicle/serviceTest/ProductTest.java

--- mpUsedVehicle.web/src/test/java/co/edu/uniandes/csw/mpusedvehicle/serviceTest/ProductTest.java
+++ mpUsedVehicle.web/src/test/java/co/edu/uniandes/csw/mpusedvehicle/serviceTest/ProductTest.java
@@ -26,6 +26,8 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
@@ -97,6 +99,8 @@ public class ProductTest {
     }
   @BeforeClass
     public static void setUp() {
+        ResteasyProviderFactory providerFactory = ResteasyProviderFactory.getInstance();
+        RegisterBuiltin.register(providerFactory);
         for (int i = 0; i < 5; i++) {
             PodamFactory factory = new PodamFactoryImpl();
             ProductDTO product = factory.manufacturePojo(ProductDTO.class);
